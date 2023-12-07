ce52deeac3a0ba451fd3ae3cd0766a6b803f6f84
luke-dvops <luketan30@.com>
2023-12-07 08:30:55 +0800

    Completed the design of API route-related test cases for resources

--- tests/routesTests.test.js
+++ tests/routesTests.test.js
@@ -8,12 +8,19 @@ chai.use(chaiHttp);
 describe('Testing API Routes', () => {
     const usersFilePath = 'utils/users.json';
     var orgContent = "";
+    const resourcesFilePath = 'utils/resources.json';
+    var orgResources = "";
+
     beforeEach(async () => {
         orgContent = await fs.readFile(usersFilePath, 'utf8');
         orgContent = JSON.parse(orgContent);
+
+        orgResources = await fs.readFile(resourcesFilePath, 'utf8');
+        orgResources = JSON.parse(orgResources);
     });
     afterEach(async () => {
         await fs.writeFile(usersFilePath, JSON.stringify(orgContent), 'utf8');
+        await fs.writeFile(resourcesFilePath, JSON.stringify(orgResources), 'utf8');
     });
 
     it('Should register a new user successfully', (done) => {
@@ -27,7 +34,7 @@ describe('Testing API Routes', () => {
                 server.close();
             });
     });
-    
+
     it('Should log in an existing user successfully', (done) => {
         chai.request(app)
             .post('/login')
@@ -39,5 +46,59 @@ describe('Testing API Routes', () => {
                 done();
                 server.close();
             });
-    })
+        it('Should add resource successfully', (done) => {
+            chai.request(app)
+                .post('/add-resource')
+                .send({
+                    name: "Sample name", location: "Some location",
+                    description: "For testing only", owner: "john@gmail.com"
+                })
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(201);
+                    expect(res.body).to.have.lengthOf(orgResources.length + 1);
+                    done();
+                    server.close();
+                });
+        });
+        it('Should retrieve resource successfully', (done) => {
+            chai.request(app)
+                .get('/view-resources')
+                .send()
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(201);
+                    expect(res.body).to.have.lengthOf(orgResources.length);
+                    done();
+                    server.close();
+                });
+        });
+        it('Should edit resource successfully', (done) => {
+            chai.request(app)
+                .put('/edit-resource/' + orgResources[0].id)
+                .send({
+                    name: "Sample name", location: "Some location",
+                    description: "For testing only", owner: "john@gmail.com"
+                })
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(201);
+                    expect(res.body.message).to.equal('Resource modified successfully!');
+                    done();
+                    server.close();
+                });
+        });
+        it('Should delete resource successfully', (done) => {
+            chai.request(app)
+                .delete('/delete-resource/' + orgResources[0].id)
+                .send()
+                .end((err, res) => {
+                    expect(err).to.be.null;
+                    expect(res).to.have.status(201);
+                    expect(res.body.message).to.equal('Resource deleted successfully!');
+                    done();
+                    server.close();
+                });
+        });
+    });
 });
